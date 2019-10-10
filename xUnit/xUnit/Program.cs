16156ac3eb6b8bb5d2c1f327fe315a233e033f07
Yana Vysotskaya <yana-v-2018@yandex.by>
2019-10-10 15:05:19 +0300

    add triangle tests

---
+++ xUnit/xUnit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xUnit
+{
+
+    public class Triangle
+    {
+        public static bool IsTriangle(double firstSide, double secondSide, double thirdSide)
+        {
+            return (firstSide + secondSide >= thirdSide && firstSide + thirdSide >= secondSide && secondSide + thirdSide >= firstSide && firstSide > 0 && secondSide > 0 && thirdSide > 0);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
