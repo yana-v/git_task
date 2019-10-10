76a102de5a07e9cc0f49fbc6ddcf285a8002feb0
Yana Vysotskaya <yana-v-2018@yandex.by>
2019-10-10 16:39:33 +0300

    add unit tests of triangles

---
+++ xUnit/xUnit/Triangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xUnit
+{
+    public class Triangle
+    {
+        public static bool IsTriangle(double firstSide, double secondSide, double thirdSide)
+        {
+            return (firstSide + secondSide >= thirdSide && firstSide + thirdSide >= secondSide && secondSide + thirdSide >= firstSide && firstSide > 0 && secondSide > 0 && thirdSide > 0);
+        }
+    }
+}
