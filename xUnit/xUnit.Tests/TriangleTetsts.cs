c6cbba62b0b7f4c0aea1d4de28ea602d3512a800
Yana Vysotskaya <yana-v-2018@yandex.by>
2019-10-17 00:32:54 +0300

    update unit tests for triangle

--- xUnit/xUnit.Tests/TriangleTetsts.cs
+++ xUnit/xUnit.Tests/TriangleTetsts.cs
@@ -5,55 +5,40 @@ namespace Tests
 {
     public class TriangleTests
     {
-        [Test]
-        public void AllSidesAreEqual()
+        private static readonly int[][] CorrectSidesForTriangle =
         {
-            Assert.IsTrue(Triangle.IsTriangle(2, 2, 2));
-        }
-        [Test]
-        public void AllSidesCannotBeZero()
+                new int[] { 2, 2, 2 },
+                new int[] { 3, 4, 5 },
+                new int[] { 4, 4, 6 }
+        };
+
+        private static readonly int[][] WrongSidesForTriangle =
         {
-            Assert.IsFalse(Triangle.IsTriangle(0, 0, 0));
-        }
-        [Test]
-        public void RightTriangle()
+                new int[] { 0, 0, 0 },
+                new int[] { 2, 0, 2 },
+                new int[] { -2, 2, 2 },
+                new int[] { -2, -2, -2 },
+                new int[] { 6, 8, 20 },
+                new int[] { 4, 4, 9 }
+        };
+
+        [TestCaseSource("WrongSidesForTriangle")]
+        public void NotValidSides(int a, int b, int c)
         {
-            Assert.IsTrue(Triangle.IsTriangle(3, 4, 5));
+            Assert.IsFalse(Triangle.IsTriangle(a, b, c));
         }
-        [Test]
-        public void SideCannotBeZero()
+        
+        [TestCaseSource("CorrectSidesForTriangle")]
+        public void ValidSides(int a, int b, int c)
         {
-            Assert.IsFalse(Triangle.IsTriangle(0, 2, 2));
+            Assert.IsTrue(Triangle.IsTriangle(a, b, c));
         }
-        [Test]
-        public void SideCannotBeLessThenZero()
-        {
-            Assert.IsFalse(Triangle.IsTriangle(-2, 2, 2));
-        }
-        [Test]
-        public void AllSidesCannotBeLessThenZero()
-        {
-            Assert.IsFalse(Triangle.IsTriangle(-2, -2, -2));
-        }
-        [Test]
-        public void SideNotLessThenSumOtherSides()
-        {
-            Assert.IsFalse(Triangle.IsTriangle(6, 8, 20));
-        }
-        [Test]
-        public void NotIntValuesOfSides()
-        {
-            Assert.IsTrue(Triangle.IsTriangle(5.5, 6.6, 7.4));
-        }
-        [Test]
-        public void IsoscelesTriangle()
-        {
-            Assert.IsTrue(Triangle.IsTriangle(4, 4, 6));
-        }
+
         [Test]
         public void NotCanBeIsoscelesTriangle()
         {
             Assert.IsFalse(Triangle.IsTriangle(4, 4, 9));
         }
+        
     }
 }
